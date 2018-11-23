a71341741f0899b707a9b7adbfc3a186bfa4de7e
tuanpx96 <tuanpx96@gmail.com>
2018-11-23 16:58:49 +0700

    create project

---
+++ gaaky/settings/dev.py
@@ -0,0 +1,8 @@
+from .base import *
+
+DEBUG = True
+GGCLOUD_REGION = 'asia-southeast1-a'
+GGCLOUD_ACCESS_KEY_ID = 'asia-southeast1-a'
+GGCLOUD_SECRET_ACCESS_KEY = 'asia-southeast1-a'
+
+EMAIL_NO_REPLY = 'tuanpx96@gmail.com'
